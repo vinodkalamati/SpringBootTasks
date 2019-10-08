278a8bce7465e5d313f1770b1d9f1bed695516d3
vinod.kalamati <vinod.kalamati@cgi.com>
2019-10-08 19:49:38 +0530

    finalcommit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,16 +2,22 @@ package com.stackroute.domain;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 @ApiModel
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Movie {
     @Id
     @ApiModelProperty(notes = "Id of the movie primary key")
+    @GeneratedValue
     int id;
     @ApiModelProperty(notes = "Title of the movie")
     String movieTitle;
@@ -20,55 +26,5 @@ public class Movie {
     @ApiModelProperty(notes = "Movie language")
     String language;
 
-    public Movie() {
-    }
 
-    public Movie(int id, String movieTitle, String overview, String language) {
-        this.id = id;
-        this.movieTitle = movieTitle;
-        this.overview = overview;
-        this.language = language;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMovieTitle() {
-        return movieTitle;
-    }
-
-    public void setMovieTitle(String movieTitle) {
-        this.movieTitle = movieTitle;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", movieTitle='" + movieTitle + '\'' +
-                ", overview='" + overview + '\'' +
-                ", language='" + language + '\'' +
-                '}';
-    }
 }
